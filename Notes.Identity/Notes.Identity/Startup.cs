8e10193aecfd97657aa11a9c19495217b80da1f3
John <wesatellite@bigmir.net>
2023-10-12 02:23:30 +0300

    Make routing mapping by controller name
    Create View with Login
    Add Styles
    
    Implementation of user login - SignInManager
    Search implementation - UserManager
    Logout implementation - IIdentityServerInteractionService
    
    Add user registration logic:
    before filling out the form and after filling out the form
    
    Add user logout logic

--- Notes.Identity/Notes.Identity/Startup.cs
+++ Notes.Identity/Notes.Identity/Startup.cs
@@ -1,15 +1,17 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 using Notes.Identity.Data;
 using Notes.Identity.Models;
+
+using System.IO;
 
 namespace Notes.Identity
 {
@@ -56,6 +58,8 @@ namespace Notes.Identity
                 config.LoginPath = "/Auth/Login";
                 config.LogoutPath = "/Auth/Logout";
             });
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,15 +70,18 @@ namespace Notes.Identity
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Styles")),
+                RequestPath = "/styles"
+            });
+
             app.UseRouting();
             app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
